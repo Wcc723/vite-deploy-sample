495d4ed9738817c75c824159e4ebfe2876b3cfa6
wcc723 <agito723@gmail.com>
2022-10-28 17:44:57 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    {{ data }}
+    <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_PATH);
+    const url = import.meta.env.VITE_PATH;
+    this.$http.get(url).then((res) => {
+      console.log(res.data);
+      this.data = res.data.results[0];
+    });
+  },
+};
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
